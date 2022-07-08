8d0115e7fdd7fbe8328b190926ec20480e9e424c
Soham Das <soham.das78@live.com>
2022-07-08 07:48:29 +0530

    batch1

---
+++ src/resumeData.js
@@ -0,0 +1,84 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Soham Das",
+    "role": "Technical Consultant",
+    "linkedinId":"SohamDas78",
+    "skypeid": "soham.das78@live.com",
+    "roleDescription": " I lead a team whose primary objective is to develop applications tailored to meet specific customer requirements.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/sohamdas78/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/SohamDas78",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/SohamDas78",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am working as a Consultant @ Microsoft India.",
+    "address":"India",
+    "website":"www.thesohamdas.com",
+    "education":[
+      {
+        "UniversityName":"KIIT - Bhubaneswar",
+        "specialization":"B.Tech (Information Technology)",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2018",
+        "Achievements":"CGPA - 8.41"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Microsoft India",
+        "specialization":"Consultant",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+        "Achievements":"Rising Star FY22 H1, Best Captain FY22 H1"
+      }
+    ],
+    "skillsDescription":"Some of my skills are listed here - ",
+    "skills":[
+      {
+        "skillname":"C# / .NET Core / Web API"
+      },
+      {
+        "skillname":"Microsoft Azure"
+      },
+      {
+        "skillname":"React.JS + Redux"
+      },
+      {
+        "skillname":"SQL Server"
+      },
+      {
+        "skillname":"Microservices"
+      },
+      {
+        "skillname":"Data Structures"
+      },
+      {
+        "skillname":"HTML5 / CSS"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"BDO Audit Process Tool Next Gen",
+        "description":"Desktop application with .NET Core based processing server",
+        "imgurl":"https://www.bdo.in/bdokit/assets/img/BDO_logo_150dpi_RGB_290709.jpg"
+      },
+      {
+        "name":"Publicis Sapient - Marcel Assistant",
+        "description":"Mobile application with cognitive capabilities",
+        "imgurl":"https://s3.amazonaws.com/media.mediapost.com/dam/cropped/2018/05/24/marcel_0hpZAF3.jpg"
+      }
+    ]
+  }
+  
+  export default resumeData
